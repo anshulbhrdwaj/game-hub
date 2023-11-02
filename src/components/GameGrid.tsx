1528d4609b1a27086b688110ef67905605d6ca1b
Anshul <umangbhardwaj0006@gmail.com>
2023-11-03 02:44:19 +0530

    Show Loading Skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,16 @@
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { error, games } = useGames();
+  const { error, games, isLoading } = useGames();
+  const skeletons = [1,2,3,4,5,6,7,8,9,10,11,12]
 
   return (
     <>
       {error && <p>{error}</p>}
       <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7 p-[3vw]">
+        {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/> )}
         {games.map((game) => (
           <GameCard game={game} key={game.id}/>
         ))}
